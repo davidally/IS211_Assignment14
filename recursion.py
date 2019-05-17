a6f82e2217c862f1e786ba1fc638092eee755bca
david <davidally94@gmail.com>
2019-05-16 20:36:56 -0400

    Refactor

--- recursion.py
+++ recursion.py
@@ -5,9 +5,16 @@
 
 def fibonnaci(n):
     if n == 0 or n == 1:
-        return 1
+        return -1
     else:
         return fibonnaci(n - 1) + fibonnaci(n - 2)
 
 
 print fibonnaci(12)
+
+
+def gcd(a, b):
+    return gcd(b, a % b) if b else abs(a)
+
+
+print gcd(10, 4)
