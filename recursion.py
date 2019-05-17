499c5a226f1021087d93338eb2d735adc48b0dbc
david <davidally94@gmail.com>
2019-05-16 20:50:18 -0400

    Typo

--- recursion.py
+++ recursion.py
@@ -17,4 +17,21 @@ def gcd(a, b):
     return gcd(b, a % b) if b else abs(a)
 
 
+# Should equal 2
 print gcd(10, 4)
+
+
+def compareTo(s1, s2):
+    if not s1 and not s2:
+        return 0
+    elif not s1:
+        return -1
+    elif not s2:
+        return 1
+    else:
+        return compareTo(s1[1:], s2[1:])
+
+
+print compareTo('pepper', 'salt')
+print compareTo('ketchup', 'ketchup')
+print compareTo('hambuger', 'frenchfries')
